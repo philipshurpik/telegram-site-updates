cb255dd2a3a358c3f5df172920ad3619f10fbeae
philipshurpik <philip.shurpik@gmail.com>
2019-08-08 13:31:02 +0300

    new reqs and config

--- config/base_config.py
+++ config/base_config.py
@@ -5,5 +5,11 @@ class Config:
     resources = {
         "olx": [
             "https://www.olx.ua/elektronika/telefony-i-aksesuary/mobilnye-telefony-smartfony/apple/kiev/q-iphone-xs-max-512gb/?search%5Bprivate_business%5D=private"
+        ],
+        "lun": [
+            "https://www.lun.ua/uk/%D0%BE%D1%80%D0%B5%D0%BD%D0%B4%D0%B0-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80-%D0%BA%D0%B8%D1%97%D0%B2-%D0%B4%D0%B2%D0%BE%D0%BA%D1%96%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D1%96?priceMax=20000"
+        ],
+        "dou": [
+            "https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Front%20End"
         ]
     }
